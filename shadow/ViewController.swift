1039f9c7802f9888f457956276c83d95ce8b1b4a
812942824 <812942824@qq.com>
2016-03-14 18:54:26 +0800

    Initial Commit

---
+++ shadow/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  shadow
+//
+//  Created by 心影 on 15/7/20.
+//  Copyright (c) 2015年 心影. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        println("hello world")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
